a2a596aaf51dd5a5042fc4b3e5a6e524cf3a7b03
nicholas.berardo <berardonicholas5@gmail.com>
2024-06-19 16:06:29 +0200

    changes

--- mask2former/modeling/pixel_decoder/ops/make.sh
+++ mask2former/modeling/pixel_decoder/ops/make.sh
@@ -9,5 +9,7 @@
 
 # Copyright (c) Facebook, Inc. and its affiliates.
 # Modified by Bowen Cheng from https://github.com/fundamentalvision/Deformable-DETR
+
+export CUDA_HOME = /share/apps/legion-modulefiles/nvidia/cudasdk/11.8_test
 source /home/nberardo/myenv/bin/activate
 python setup.py build install
