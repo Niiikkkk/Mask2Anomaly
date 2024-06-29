ce62bd9bd58d9e860bda739c13480345ef0e3f4c
nicholas.berardo <berardonicholas5@gmail.com>
2024-06-29 08:47:20 +0200

    changes

--- mask2former/data/dataset_mappers/mask_former_semantic_dataset_mapper.py
+++ mask2former/data/dataset_mappers/mask_former_semantic_dataset_mapper.py
@@ -196,11 +196,9 @@ class MaskFormerSemanticDatasetMapper:
         assert self.is_train, "MaskFormerSemanticDatasetMapper should only be used for training!"
 
         dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
-        print("\n\n\nCIAOOO\n\n\n", file=sys.stderr)
-        print(self.img_format,file=sys.stderr)
-        print("\n", self.is_ood_ft, file=sys.stderr)
-        sys.stderr.flush()
         image = utils.read_image(dataset_dict["file_name"], format=self.img_format)
+        print(image.flags,file=sys.stderr)
+        sys.stderr.flush()
         utils.check_image_size(dataset_dict, image)
 
         if "sem_seg_file_name" in dataset_dict:
