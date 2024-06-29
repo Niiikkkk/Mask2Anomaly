a762a8db2a2dc1e53196a871ab85ef9af8402fa5
nicholas.berardo <berardonicholas5@gmail.com>
2024-06-29 08:39:12 +0200

    changes

--- mask2former/data/dataset_mappers/mask_former_semantic_dataset_mapper.py
+++ mask2former/data/dataset_mappers/mask_former_semantic_dataset_mapper.py
@@ -196,6 +196,9 @@ class MaskFormerSemanticDatasetMapper:
         assert self.is_train, "MaskFormerSemanticDatasetMapper should only be used for training!"
 
         dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+        print("\n\n\nCIAOOO\n\n\n", file=sys.stderr)
+        print(self.img_format,file=sys.stderr)
+        print("\n", self.is_ood_ft, file=sys.stderr)
         image = utils.read_image(dataset_dict["file_name"], format=self.img_format)
         utils.check_image_size(dataset_dict, image)
 
@@ -213,8 +216,6 @@ class MaskFormerSemanticDatasetMapper:
             )
 
         #Anomaly Mix#
-        print("\n\n\nCIAOOO\n\n\n",file=sys.stderr)
-        print("\n",self.is_ood_ft,file=sys.stderr)
         if self.is_ood_ft: 
             if np.random.uniform() < self.anomaly_mix_ratio:
                 coco_gt_path = random.choice(self.gt_list)
